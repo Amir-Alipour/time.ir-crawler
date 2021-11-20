e98b43853ee319529013e704eff1ef2a95d89e1e
amir alipour <amir.alipour12127@gmail.com>
2021-11-20 11:23:58 +0330

    fix

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const cheerio = require("cheerio");
 
 app.use(express.static("public"));
 
-app.get("/month", async (req, res) => {
+app.get("/date", async (req, res) => {
   let pusher = [];
   let qoute = {};
   let events = [];
@@ -62,7 +62,7 @@ app.get("/month", async (req, res) => {
 // || ========================================== ||
 
 app.get("/year", async (req, res) => {
-  let events = {};
+  let events = [];
 
   await request(
     "https://www.time.ir/fa/eventyear-%D8%AA%D9%82%D9%88%DB%8C%D9%85-%D8%B3%D8%A7%D9%84%DB%8C%D8%A7%D9%86%D9%87",
