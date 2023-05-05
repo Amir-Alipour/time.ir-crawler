a4f768a4089c920e9c9197292e153fa5a151bfc0
amir alipour <amir.alipour12127@gmail.com>
2023-05-05 15:45:02 +0330

    change structure

--- index.js
+++ index.js
@@ -1,162 +1,81 @@
 const express = require("express");
 const app = express();
 const port = 3001;
-const request = require("request-promise");
-const cheerio = require("cheerio");
+
+// =============== Crawlers ================
+const crawler = require("./crawlers/crawler");
+const getDate = require("./crawlers/date");
+const getMonth = require("./crawlers/month");
+const getYear = require("./crawlers/year");
+const getQoute = require("./crawlers/qoute");
+const getBooks = require("./crawlers/books");
+// =========================================
 
 app.use(express.static("public"));
 
-// GET TODAY AND CURRENT MONTH DETAILS AND RANDOM QOUTE
+// GET TODAY
 app.get("/date", async (req, res) => {
-  let pusher = [];
-  let qoute = {};
-  let events = [];
-
-  await request("https://www.time.ir/", async (error, response, html) => {
-    if (!error && response.statusCode == 200) {
-      const $ = cheerio.load(html);
-
-      let names = ["shamsi", "gregorian", "hijri"];
-
-      names.map((name) => {
-        let mode = $(`.today-${name}`).first().find(".title").text();
-        let mode_date = $(`.today-${name}`).first().find(".numeral").text();
-        let mode_date_text = $(`.today-${name}`).first().find(".date").text();
-
-        pusher.push({
-          mode: mode,
-          date: mode_date,
-          date_text: mode_date_text,
+    crawler(URLs.date, getDate).then((data) => {
+        res.send({
+            dates: data,
         });
-      });
-      // -----------
-
-      qoute.text = $(".randomQuote").find(".quoteText").text();
-      qoute.auth = $(".randomQuote").find(".quoteAuthor").text();
-      qoute.auth_link = $(".randomQuote").find(".quoteAuthor").attr("href");
-      // -----------
-
-      $(".eventsCurrentMonthWrapper")
-        .find("> ul")
-        .find("li")
-        .each((i, data) => {
-          let isHoliday = $(data).hasClass("eventHoliday");
-          let date = $(data).find("> span").first().text();
-          let event = $(data).clone().children().remove().end().text().trim();
-
-          events.push({
-            date,
-            event,
-            isHoliday,
-          });
-        });
-    }
-  });
-
-  res.send({
-    dates: pusher,
-    qoute,
-    event_current_month: events,
-  });
+    });
 });
 // --------------------
-// || ========================================== ||
+
+// GET CURRENT MONTH DETAILS
+app.get("/month", async (req, res) => {
+    crawler(URLs.month, getMonth).then((data) => {
+        res.send({
+            current_month: data.current,
+            current_month_iranian_events: data.events,
+        });
+    });
+});
+// --------------------
 
 // GET CURRENT YEAR EVENTS
 app.get("/year", async (req, res) => {
-  let events = [];
-
-  await request(
-    "https://www.time.ir/fa/eventyear-%D8%AA%D9%82%D9%88%DB%8C%D9%85-%D8%B3%D8%A7%D9%84%DB%8C%D8%A7%D9%86%D9%87",
-    async (error, response, html) => {
-      if (!error && response.statusCode == 200) {
-        const $ = cheerio.load(html);
-
-        $(".eventsCurrentMonthWrapper").each((i, data) => {
-          let month_name = $(data).find("span").first().text().trim();
-          let month_events = [];
-
-          $(data)
-            .find("> ul")
-            .find("li")
-            .each((i, data) => {
-              let isHoliday = $(data).hasClass("eventHoliday");
-              let date = $(data).find("> span").first().text();
-              let event = $(data)
-                .clone()
-                .children()
-                .remove()
-                .end()
-                .text()
-                .trim();
-
-              month_events.push({
-                date,
-                event,
-                isHoliday,
-              });
-            });
-
-          events.push({
-            month_name,
-            events: month_events,
-          });
+    crawler(URLs.year, getYear).then((data) => {
+        res.send({
+            current_year_iranian_events: data,
         });
-      }
-    }
-  );
-
-  res.send({
-    year_events: events,
-  });
+    });
 });
 // --------------------
-// || ========================================== ||
 
-// GET LIST OF BOOKS AND RANDOM QOUTE
-app.get("/book", async (req, res) => {
-  let books = [];
-  let qoute = {};
-
-  await request(`http://localhost:${port}/date`, (err, response) => {
-    qoute = JSON.parse(response.body).qoute;
-  });
-
-  await request(
-    "https://www.time.ir/fa/booklist-%d9%81%d9%87%d8%b1%d8%b3%d8%aa-%da%a9%d8%aa%d8%a7%d8%a8%d9%87%d8%a7%db%8c-%d9%85%d8%b9%d8%b1%d9%81%db%8c-%d8%b4%d8%af%d9%87-%d8%af%d8%b1-%d8%aa%d8%a7%db%8c%d9%85",
-    async (error, response, html) => {
-      if (!error && response.statusCode == 200) {
-        const $ = cheerio.load(html);
-
-        $(".panel").each((i, data) => {
-          if (i !== 0) {
-            let title = $(data).find(".box-title").text();
-            let thumnail = $(data).find(".content img").attr("src");
-            let description = $(data).find(".content").text();
-
-            books.push({
-              title,
-              thumnail,
-              description,
-            });
-          }
+// GET random qoute
+app.get("/qoute", async (req, res) => {
+    crawler(URLs.qoute, getQoute).then((data) => {
+        res.send({
+            qoute: data,
         });
-      }
-    }
-  );
-
-  res.send({
-    qoute,
-    books,
-  });
+    });
 });
 // --------------------
-// || ========================================== ||
+
+// GET LIST OF BOOKS
+app.get("/books", async (req, res) => {
+    crawler(URLs.books, getBooks).then((data) => {
+        res.send({
+            books: data,
+        });
+    });
+});
+// --------------------
 
 app.get("/", (req, res) => {
-  res.send("server is running");
+    res.send("server is running");
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+    console.log(`Example app listening at http://localhost:${port}`);
 });
+
+const URLs = {
+    date: "/",
+    month: "/",
+    year: "/fa/eventyear-%D8%AA%D9%82%D9%88%DB%8C%D9%85-%D8%B3%D8%A7%D9%84%DB%8C%D8%A7%D9%86%D9%87",
+    qoute: "/",
+    books: "/fa/booklist-%d9%81%d9%87%d8%b1%d8%b3%d8%aa-%da%a9%d8%aa%d8%a7%d8%a8%d9%87%d8%a7%db%8c-%d9%85%d8%b9%d8%b1%d9%81%db%8c-%d8%b4%d8%af%d9%87-%d8%af%d8%b1-%d8%aa%d8%a7%db%8c%d9%85",
+};
