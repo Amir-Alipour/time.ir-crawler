3f83277c4362f5fd51a5b8f2594d10c7e8f154f5
amir alipour <amir.alipour12127@gmail.com>
2023-05-07 13:58:54 +0330

    optimize the structure

---
+++ ROUTES.js
@@ -0,0 +1,69 @@
+// =============== Crawlers ================
+const getDate = require("./crawlers/date");
+const getMonth = require("./crawlers/month");
+const getYear = require("./crawlers/year");
+const getQoute = require("./crawlers/qoute");
+const getBooks = require("./crawlers/books");
+// =========================================
+
+const URLs = {
+    date: "/",
+    month: "/",
+    year: "/fa/eventyear-%D8%AA%D9%82%D9%88%DB%8C%D9%85-%D8%B3%D8%A7%D9%84%DB%8C%D8%A7%D9%86%D9%87",
+    qoute: "/",
+    books: "/fa/booklist-%d9%81%d9%87%d8%b1%d8%b3%d8%aa-%da%a9%d8%aa%d8%a7%d8%a8%d9%87%d8%a7%db%8c-%d9%85%d8%b9%d8%b1%d9%81%db%8c-%d8%b4%d8%af%d9%87-%d8%af%d8%b1-%d8%aa%d8%a7%db%8c%d9%85",
+};
+
+exports.ROUTES = [
+    {
+        route: "/date",
+        crawler: getDate,
+        crawler_url: URLs.date,
+        response: (data) => {
+            return {
+                dates: data,
+            };
+        },
+    },
+    {
+        route: "/month",
+        crawler: getMonth,
+        crawler_url: URLs.month,
+        response: (data) => {
+            return {
+                current_month: data.current,
+                current_month_iranian_events: data.events,
+            };
+        },
+    },
+    {
+        route: "/year",
+        crawler: getYear,
+        crawler_url: URLs.year,
+        response: (data) => {
+            return {
+                current_year_iranian_events: data,
+            };
+        },
+    },
+    {
+        route: "/qoute",
+        crawler: getQoute,
+        crawler_url: URLs.qoute,
+        response: (data) => {
+            return {
+                qoute: data,
+            };
+        },
+    },
+    {
+        route: "/books",
+        crawler: getBooks,
+        crawler_url: URLs.books,
+        response: (data) => {
+            return {
+                books: data,
+            };
+        },
+    },
+];
--- index.js
+++ index.js
@@ -2,67 +2,24 @@ const express = require("express");
 const app = express();
 const port = 3001;
 
-// =============== Crawlers ================
 const crawler = require("./crawlers/crawler");
-const getDate = require("./crawlers/date");
-const getMonth = require("./crawlers/month");
-const getYear = require("./crawlers/year");
-const getQoute = require("./crawlers/qoute");
-const getBooks = require("./crawlers/books");
-// =========================================
+const { ROUTES } = require("./ROUTES");
 
 app.use(express.static("public"));
 
-// GET TODAY
-app.get("/date", async (req, res) => {
-    crawler(URLs.date, getDate).then((data) => {
-        res.send({
-            dates: data,
-        });
-    });
-});
-// --------------------
 
-// GET CURRENT MONTH DETAILS
-app.get("/month", async (req, res) => {
-    crawler(URLs.month, getMonth).then((data) => {
-        res.send({
-            current_month: data.current,
-            current_month_iranian_events: data.events,
-        });
-    });
-});
-// --------------------
+// --------------- crawlers Routes ------------------
+for (let index = 0; index < ROUTES.length; index++) {
+    const route = ROUTES[index];
 
-// GET CURRENT YEAR EVENTS
-app.get("/year", async (req, res) => {
-    crawler(URLs.year, getYear).then((data) => {
-        res.send({
-            current_year_iranian_events: data,
+    app.get(route.route, async (_, res) => {
+        crawler(route.crawler_url, route.crawler).then((data) => {
+            res.send(route.response(data));
         });
     });
-});
-// --------------------
+}
+// --------------- --------------- ------------------
 
-// GET random qoute
-app.get("/qoute", async (req, res) => {
-    crawler(URLs.qoute, getQoute).then((data) => {
-        res.send({
-            qoute: data,
-        });
-    });
-});
-// --------------------
-
-// GET LIST OF BOOKS
-app.get("/books", async (req, res) => {
-    crawler(URLs.books, getBooks).then((data) => {
-        res.send({
-            books: data,
-        });
-    });
-});
-// --------------------
 
 app.get("/", (req, res) => {
     res.send("server is running");
@@ -71,11 +28,3 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
 });
-
-const URLs = {
-    date: "/",
-    month: "/",
-    year: "/fa/eventyear-%D8%AA%D9%82%D9%88%DB%8C%D9%85-%D8%B3%D8%A7%D9%84%DB%8C%D8%A7%D9%86%D9%87",
-    qoute: "/",
-    books: "/fa/booklist-%d9%81%d9%87%d8%b1%d8%b3%d8%aa-%da%a9%d8%aa%d8%a7%d8%a8%d9%87%d8%a7%db%8c-%d9%85%d8%b9%d8%b1%d9%81%db%8c-%d8%b4%d8%af%d9%87-%d8%af%d8%b1-%d8%aa%d8%a7%db%8c%d9%85",
-};
