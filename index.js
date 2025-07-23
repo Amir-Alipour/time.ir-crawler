f8af7009fdd1dd24fadf631e159a292cb433b9f4
Amir Alipour <amir1.alipour@gmail.com>
2025-07-23 11:36:51 +0330

    fix: port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 3001;
+const port = 3000;
 
 const crawler = require("./crawlers/crawler");
 const { ROUTES } = require("./ROUTES");
